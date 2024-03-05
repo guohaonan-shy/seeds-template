5bcd252459d1c3ed6da4e878ddfbc403adac0408
Aatrox <guohaonan980421@163.com>
2024-03-05 11:22:52 +0800

    [feat]add customer

---
+++ customer_management/lakeside_customer_add_customer_seeds.py
@@ -0,0 +1,102 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class LakesideAddCustomerSeeds:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def execute_seeds(self, email, password, first_name, last_name, birth, street_address, postal_code, city,
+                      phone):
+        driverIns = self.driver
+
+        # starting
+        # click add button
+        print("Click on 'Search' button")
+        new_customer_button = driverIns.find_element(By.CSS_SELECTOR, 'a[href="/customers/new"]')
+        new_customer_button.click()
+
+        self.edit_new_customer(email, password, first_name, last_name, birth, street_address, postal_code, city,
+                               phone)
+
+    def edit_new_customer(self, email, password, first_name, last_name, birth, street_address, postal_code, city,
+                          phone):
+        driverIns = self.driver
+
+        # here, users are staying in the new customers' page
+        # complementing the form
+        form = driverIns.find_element(By.CSS_SELECTOR, 'form[class="ui form"]')
+
+        # email_address
+        email_input = form.find_element(By.CSS_SELECTOR, 'input[name="email"]')
+        if email != "":
+            email_input.clear()
+            email_input.send_keys(email)
+            print(email_input.get_attribute("value"))
+
+        # password
+        password_input = form.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+        if password != "":
+            password_input.clear()
+            password_input.send_keys(password)
+            print(password_input.get_attribute("value"))
+
+        # first_name
+        first_name_input = form.find_element(By.CSS_SELECTOR, 'input[name="firstname"]')
+        if first_name_input != "":
+            first_name_input.clear()
+            first_name_input.send_keys(first_name)
+            print(first_name_input.get_attribute("value"))
+
+        # last_name
+        last_name_input = form.find_element(By.CSS_SELECTOR, 'input[name="lastname"]')
+        if last_name_input != "":
+            last_name_input.clear()
+            last_name_input.send_keys(last_name)
+            print(last_name_input.get_attribute("value"))
+
+        # birthday
+        birthday_input = form.find_element(By.CSS_SELECTOR, 'input[name="birthday"]')
+        if birth != "":
+            birthday_input.clear()
+            dates = birth.split("-")
+            birthday_input.send_keys(dates[0])
+            birthday_input.send_keys(Keys.RIGHT)
+            birthday_input.send_keys(dates[1])
+            birthday_input.send_keys(dates[2])
+            print(birthday_input.get_attribute("value"))
+
+        # street_address
+        street_address_input = form.find_element(By.CSS_SELECTOR, 'input[name="streetAddress"]')
+        if street_address != "":
+            street_address_input.clear()
+            street_address_input.send_keys(street_address)
+            print(street_address_input.get_attribute("value"))
+
+        # postal_code
+        postal_code_input = form.find_element(By.CSS_SELECTOR, 'input[name="postalCode"]')
+        if postal_code != "":
+            postal_code_input.clear()
+            postal_code_input.send_keys(postal_code)
+            print(postal_code_input.get_attribute("value"))
+
+        # city_code
+        city_input = form.find_element(By.CSS_SELECTOR, 'input[name="city"]')
+        if city != "":
+            city_input.clear()
+            city_input.send_keys(city)
+            print(city_input.get_attribute("value"))
+
+        # phone_number
+        phone_input = form.find_element(By.CSS_SELECTOR, 'input[name="phoneNumber"]')
+        if phone != "":
+            phone_input.clear()
+            phone_input.send_keys(phone)
+            print(phone_input.get_attribute("value"))
+
+        # click crete customer button
+        button = form.find_element(By.CSS_SELECTOR, 'button[class="ui blue fluid button"]')
+        button.click()
--- customer_management/test.py
+++ customer_management/test.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from selenium import webdriver
@@ -5,6 +6,7 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 import utils
+from customer_management.lakeside_customer_add_customer_seeds import LakesideAddCustomerSeeds
 from customer_management.lakeside_edit_customer_profile_seeds import LakesideEditCustomerProfileSeeds
 from customer_management.lakeside_fetch_customer_seeds import LakeSideFetchProfileSeeds
 from customer_management.lakeside_search_customers_seeds import LakesideSearchSeeds
@@ -23,6 +25,18 @@ class UpdateDto:
         self.phone_number = phone_number
         return
 
+class AddCustomerDto:
+    def __init__(self, email, password, first_name, last_name, birth, street_address, postal_code, city,
+                      phone):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth = birth
+        self.street_address = street_address
+        self.postal_code = postal_code
+        self.city = city
+        self.phone = phone
 
 class MyTestCase(unittest.TestCase):
     def test_search_true(self):
@@ -231,6 +245,52 @@ class MyTestCase(unittest.TestCase):
 
     def test_converDay(self):
         utils.convertDay("1st")
+
+    def test_add_customer(self):
+        # 1. add a new customer
+        options = webdriver.ChromeOptions()
+        options.add_argument('__no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--hide-scrollbars')
+
+        driver = webdriver.Chrome(options=options)
+
+        driver.set_window_size(1920, 967)
+
+        driver.get("http://localhost:3020")
+
+        add_customer_seed = LakesideAddCustomerSeeds(driver)
+
+        # keep a struct to store info which is used to compare with db
+        case_random_number = random.randint(a= 1000000, b=9999999)
+
+        testDto = AddCustomerDto(email="guohaonan{}@example.com".format(case_random_number), password=case_random_number, first_name="Haonan{}".format(case_random_number), last_name="Guo", birth="1998-04-21",
+                                 street_address="redhill {}".format(case_random_number), postal_code=random.randint(1000, 9999), city="Singapore", phone="+86 {} {}".format(random.randint(0000,9999), random.randint(0000,9999)))
+
+        add_customer_seed.execute_seeds(email= testDto.email, password=testDto.password, first_name=testDto.first_name, last_name=testDto.last_name, birth=testDto.birth,
+                                        street_address=testDto.street_address, postal_code=testDto.postal_code, city=testDto.city, phone=testDto.phone)
+
+        # compare and assert that the customer info is the same with expectation
+        search_seeds = LakesideSearchSeeds(driver)
+        target = "{} {}".format(testDto.first_name, testDto.last_name)
+        search_seeds.search(testDto.first_name, target)
+
+        # handle search result
+        fetchSeed = LakeSideFetchProfileSeeds(driver)
+        profile = fetchSeed.execute_seeds(target)
+
+        for key, value in profile.items():
+            if key == "Name":
+                self.assertEqual(value, target)
+            elif key == "Address":
+                self.assertEqual(value, "{}, {} {}".format(testDto.street_address, testDto.postal_code, testDto.city))
+            elif key == "Date of Birth":
+                self.assertEqual(value, "April 21st 1998")
+            elif key == "Email Address":
+                self.assertEqual(value, testDto.email)
+            elif key == "Phone Number":
+                self.assertEqual("+86 {}".format(value), testDto.phone)
+
 
 if __name__ == '__main__':
     unittest.main()

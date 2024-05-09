72a7d41b7739d506dca559208830b6769251b49a
Aatrox <guohaonan980421@163.com>
2024-05-09 19:30:15 +0800

    [feat]commit

---
+++ nicefish_seeds/comment_delete_seeds.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class NicefishCommentDeleteSeeds:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def jump_to_comment_management_page(self):
+        setting_button = self.driver.find_element(By.CSS_SELECTOR, 'a[class="nav-link"][href="/manage/chart"]')
+        setting_button.click()
+        time.sleep(2)
+        print("direct to setting page......")
+
+        post_table_button = self.driver.find_element(By.CSS_SELECTOR,
+                                                     'a[class="list-group-item"][href="/manage/comment-table"]')
+        post_table_button.click()
+        time.sleep(2)
+        print("direct to comment management......")
+
+    def execute_seeds(self):
+        rows = self.driver.find_elements(By.CSS_SELECTOR, 'tr[role="row"]')
+        # the first one
+        target = rows[0]
+        delete_button = target.find_element(By.CSS_SELECTOR,
+                                            'button[class="p-button p-component p-button-danger p-button-icon-only"]')
+
+        delete_button.click()
+        time.sleep(2)
+        print("delete success......")
